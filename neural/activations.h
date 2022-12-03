103766e28898e65fb4ab561f1d9ff1069fdd8459
Linus-J <74065299+Linus-J@users.noreply.github.com>
2022-12-03 19:25:29 +0000

    Delete neural directory

--- neural/activations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../matrix/matrix.h"
-
-double sigmoid(double input);
-double relu(double input);
-Matrix* sigmoidPrime(Matrix* m);
-Matrix* reluPrime(Matrix* m);
-Matrix* softmax(Matrix* m);
